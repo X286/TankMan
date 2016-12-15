f535216a17387625e2af285ef64b8b45f9113877
Sergey_NG <operatorsn@domofon.net>
2016-12-15 18:25:37 +0300

    Simple scrolling in process

--- Mech/Mech.py
+++ Mech/Mech.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-class Movement (object):
+class Movement(object):
     def __init__(self, speedX, speedY =0):
         if speedY == 0:
             speedY = speedX
@@ -19,14 +19,21 @@ class Movement (object):
     def move_down(self):
         self.rect.y += self.speedY
 
+    def set_speed(self, speedX, speedY=0):
+        if speedY == 0:
+            speedY = speedX
+        self.speedX = speedX
+        self.speedY = speedY
 
-# скроллинг без участия игрока, всегда вверх, например
+    def get_speed(self):
+        return self.speedX, self.speedY
+
+# скроллинг без участия игрока,
 # тут *all - тут смещение всех элеметов, которые мы хотим сместить
-class shoot_em_up_scroll (Movement):
+class ShootEmUpScroll(Movement):
     def __init__(self, speedX, speedY =0, *all):
         self.all = all
-        super(shoot_em_up_scroll, self).__init__(speedX, speedY)
-
+        super(ShootEmUpScroll, self).__init__(speedX, speedY)
 
     def move_left(self):
         for iter, idx in enumerate (self.all):
@@ -41,24 +48,61 @@ class shoot_em_up_scroll (Movement):
     def move_up(self):
         for iter, idx in enumerate(self.all):
             for one in self.all[iter]:
-                one.y -= self.speedY
+                one.rect.y -= self.speedY
 
     def move_down(self):
         for iter, idx in enumerate(self.all):
             for one in self.all[iter]:
-                one.y += self.speedY
+                one.rect.y += self.speedY
 
 # Простой скроллинг, движение если блок приблизился к экрану
 # тут старт скроллинг - это когда начинать скроллинг
 # игровой спрайт - игроки
 # screen основное окно
-class Scrolling_Simple (Movement):
-    def __init__(self, start_crollingX, start_crollingY, speedX, screen,  speedY=0, *playerSprite):
-        super(Scrolling_Simple, self).__init__(speedX, speedY)
-        self.start_crollingX = start_crollingX
-        self.start_crollingY = start_crollingY
+class ScrollingSimple(Movement):
+    def __init__(self, scrollingX, scrollingY, speedX, players, *all):
+        super(ScrollingSimple, self).__init__(speedX, speedY=0)
+        print type (scrollingX)
+        if (not (type (scrollingX) is list)and (type (scrollingY)is list)):
+            raise SyntaxError (u'[X][Y], pos coords in list')
+        self.scrollingX = scrollingX
+        self.scrollingY = scrollingY
+        self.all = all
+        self.players = players
+        print self.players
 
+    def move_left(self):
+        for player in self.players:
+
+            if self.scrollingX[0] < player.rect.x:
+                for group in self.all:
+                    for objects, index in enumerate(group):
+                        index.move_left()
+                        return True
+            else:
+                return False
+        # not needed
+        #else:
+        #    for player in self.players:
+        #        player.move_right()
+
+
+    def move_right(self):
+        if self.scrollingX[0] > self.players[0].rect.x:
+            for group in self.all:
+                for objects, index in enumerate(group):
+                    index.move_left()
+                    # if players two
+        else:
+            for player in self.players:
+                player.move_right()
+
+    def move_up(self):
+        pass
+
+    def move_down(self):
+        pass
 
 # Скроллинг - прямоугольник т.е. персонаж бегает в каком то прямоугольнике и
 # пересекая его, начинается скроллинг основной сцены
-class Scroll_Rect (object): pass
+class ScrollRect(Movement): pass
--- main.py
+++ main.py
@@ -6,28 +6,50 @@ import Mech
 def main ():
     screen = pygame.display.set_mode((800, 600))
     BrickBlock = GameObject.AnimatedObject(3, 100, 100, 50, 50)
+    BrickBlock1 = GameObject.AnimatedObject(3, 150, 150, 50, 50)
     bg = GameObject.Static_BG(3, 0, 0, 800, 600)
     bg.LoadImage('res/bg/ccc.jpg')
     bgG = Graphics.BaseObj.UniteSprite (bg)
-    bgGMove = Mech.Mech.shoot_em_up_scroll(3, 0, bgG)
+    bgGMove = Mech.Mech.ScrollingSimple([640, 100], [480,100], 3, [BrickBlock, BrickBlock1], bgG)
     pygame.init()
     MoveFigure_down = pygame.USEREVENT + 1
-    pygame.time.set_timer(MoveFigure_down, 350)
+    pygame.time.set_timer(MoveFigure_down, 50)
     FPS = pygame.time.Clock()
     isWorking=True
     while (isWorking):
         FPS.tick(60)
+        key = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 isWorking = False
-            if event.type == MoveFigure_down:
+            #if event.type == MoveFigure_down:
+                #bgGMove.move_down()
+
+
+        if key[pygame.K_RIGHT]:
+            scroll = bgGMove.move_left()
+            if scroll == False:
+                BrickBlock.move_right()
+                #BrickBlock1.move_right()
+            '''
+            if bgGMove.start_crollingX[0] < bgGMove.players[0].rect.x:
                 bgGMove.move_left()
 
+            else:
+                BrickBlock.move_right()
+            '''
+        elif key[pygame.K_LEFT]:
+            '''
+            if bgGMove.start_crollingX[1] > bgGMove.players[0].rect.x:
+                bgGMove.move_right()
 
-
+            else:
+                BrickBlock.move_left()
+            '''
         screen.fill(pygame.Color('#000000'))
         bgG.draw(screen)
         BrickBlock.draw(screen)
+        BrickBlock1.draw(screen)
         pygame.display.flip()
 
 
