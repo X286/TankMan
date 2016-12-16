153b7c8dffaf2068d6b7ba1785d7c3af22cd0eb3
Sergey_NG <operatorsn@domofon.net>
2016-12-16 16:15:40 +0300

    Simple scrolling done

--- Mech/Mech.py
+++ Mech/Mech.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 
+
 class Movement(object):
     def __init__(self, speedX, speedY =0):
         if speedY == 0:
@@ -28,6 +29,7 @@ class Movement(object):
     def get_speed(self):
         return self.speedX, self.speedY
 
+
 # скроллинг без участия игрока,
 # тут *all - тут смещение всех элеметов, которые мы хотим сместить
 class ShootEmUpScroll(Movement):
@@ -55,53 +57,85 @@ class ShootEmUpScroll(Movement):
             for one in self.all[iter]:
                 one.rect.y += self.speedY
 
+
 # Простой скроллинг, движение если блок приблизился к экрану
 # тут старт скроллинг - это когда начинать скроллинг
 # игровой спрайт - игроки
 # screen основное окно
 class ScrollingSimple(Movement):
-    def __init__(self, scrollingX, scrollingY, speedX, players, *all):
+    def __init__(self, scrollingX, scrollingY, speedX, players, all):
         super(ScrollingSimple, self).__init__(speedX, speedY=0)
-        print type (scrollingX)
         if (not (type (scrollingX) is list)and (type (scrollingY)is list)):
             raise SyntaxError (u'[X][Y], pos coords in list')
         self.scrollingX = scrollingX
         self.scrollingY = scrollingY
         self.all = all
         self.players = players
-        print self.players
 
     def move_left(self):
+        in_board = 0
         for player in self.players:
-
             if self.scrollingX[0] < player.rect.x:
-                for group in self.all:
-                    for objects, index in enumerate(group):
-                        index.move_left()
-                        return True
-            else:
-                return False
-        # not needed
-        #else:
-        #    for player in self.players:
-        #        player.move_right()
-
+                in_board +=1
+        if in_board == len(self.players):
+            for group in self.all:
+                for item in group:
+                    item.move_left()
+            return True
+        else:
+            return False
 
     def move_right(self):
-        if self.scrollingX[0] > self.players[0].rect.x:
+
+        in_board = 0
+        for player in self.players:
+            if self.scrollingX[1] > player.rect.x > 0:
+                in_board += 1
+        if in_board == len(self.players):
             for group in self.all:
-                for objects, index in enumerate(group):
-                    index.move_left()
-                    # if players two
+                for item in group:
+                    item.move_right()
+            return True
         else:
-            for player in self.players:
-                player.move_right()
+            return False
 
     def move_up(self):
-        pass
+        in_board = 0
+        for player in self.players:
+            if self.scrollingY[0] > player.rect.y > 0:
+                in_board += 1
+        if in_board == len(self.players):
+                for group in self.all:
+                    for item in group:
+                        item.move_down()
+                return True
+        else:
+                return False
 
     def move_down(self):
-        pass
+        in_board = 0
+        for player in self.players:
+            if self.scrollingY[1] < player.rect.y :
+                in_board += 1
+        if in_board == len(self.players):
+                for group in self.all:
+                    for item in group:
+                        item.move_up()
+                return True
+        else:
+            return False
+
+
+    # может скроллить, если все игроки находятся в секторе скроллинга
+    #[Вверх вверх, вправо влево]
+    def scroll (self):
+        merge = []
+        merge.append(self.move_down())
+        merge.append(self.move_up())
+        merge.append(self.move_right())
+        merge.append(self.move_left())
+        return merge
+
 
 # Скроллинг - прямоугольник т.е. персонаж бегает в каком то прямоугольнике и
 # пересекая его, начинается скроллинг основной сцены
--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import Mech
 def main ():
     screen = pygame.display.set_mode((800, 600))
     BrickBlock = GameObject.AnimatedObject(3, 100, 100, 50, 50)
-    BrickBlock1 = GameObject.AnimatedObject(3, 150, 150, 50, 50)
+    BrickBlock1 = GameObject.AnimatedObject(3, 50, 490, 50, 50)
     bg = GameObject.Static_BG(3, 0, 0, 800, 600)
     bg.LoadImage('res/bg/ccc.jpg')
     bgG = Graphics.BaseObj.UniteSprite (bg)
-    bgGMove = Mech.Mech.ScrollingSimple([640, 100], [480,100], 3, [BrickBlock, BrickBlock1], bgG)
+    bgGMove = Mech.Mech.ScrollingSimple([640, 100], [100, 480], 3, [BrickBlock, BrickBlock1], [bgG])
     pygame.init()
     MoveFigure_down = pygame.USEREVENT + 1
     pygame.time.set_timer(MoveFigure_down, 50)
     FPS = pygame.time.Clock()
     isWorking=True
+
     while (isWorking):
         FPS.tick(60)
         key = pygame.key.get_pressed()
@@ -24,29 +25,26 @@ def main ():
                 isWorking = False
             #if event.type == MoveFigure_down:
                 #bgGMove.move_down()
-
-
+        poses = bgGMove.scroll()
         if key[pygame.K_RIGHT]:
-            scroll = bgGMove.move_left()
-            if scroll == False:
-                BrickBlock.move_right()
-                #BrickBlock1.move_right()
-            '''
-            if bgGMove.start_crollingX[0] < bgGMove.players[0].rect.x:
-                bgGMove.move_left()
 
-            else:
+            if poses[3] == False:
                 BrickBlock.move_right()
-            '''
-        elif key[pygame.K_LEFT]:
-            '''
-            if bgGMove.start_crollingX[1] > bgGMove.players[0].rect.x:
-                bgGMove.move_right()
 
-            else:
+        if key[pygame.K_LEFT]:
+            if poses[2] == False:
                 BrickBlock.move_left()
-            '''
+
+        if key[pygame.K_UP]:
+            if poses[1] == False:
+                BrickBlock.move_up()
+
+        if key[pygame.K_DOWN]:
+            if poses[0] == False:
+                BrickBlock.move_down()
+
         screen.fill(pygame.Color('#000000'))
+
         bgG.draw(screen)
         BrickBlock.draw(screen)
         BrickBlock1.draw(screen)
