8c4b4c053761c0ee259295c37cdc869072d97972
X286 <goryunov.sn@gmail.com>
2017-01-26 18:19:41 +0300

    added sprite

--- Parcer/CreateTIleMap.py
+++ Parcer/CreateTIleMap.py
@@ -50,3 +50,54 @@ class CreateTileMap(object):
                     ragnar.append(whithout_counter.group(0))
             splittedMapa.append(ragnar)
         return splittedMapa
+
+class CreateSptitesOnMap(object):
+
+    def __init__(self, spritepathf):
+        self.sptiteplace = []
+        try:
+            self.sprtf = open(spritepathf, 'r')
+        except:
+            raise IOError ('Coud not open sprite file' + spritepathf)
+        self.__syntax_check()
+
+    def __syntax_check (self):
+        start = False
+        end = False
+        for line in self.sprtf:
+            line = line.replace('\t', '')
+            line = line.replace('\n', '')
+            line = line.replace(' ', '')
+            if line == '[':
+                start = True
+            elif line == ']':
+                end = True
+            else:
+                matched = re.match('[a-zA-Z\{0-9\}, ]+;', line)
+                if matched != None:
+                    getSpriteString = matched.group(0).replace(';', '')
+                    self.sptiteplace.append(getSpriteString)
+                else:
+                    raise SyntaxError('Missed statement in block lower block ' + getSpriteString)
+        if (start is False) or (end is False):
+            raise SyntaxError('missed start block or end block')
+
+    def SpritePlaced(self):
+        splittedSprite = {}
+        for line in self.sptiteplace:
+            valid_value = re.match('([a-zA-Z][A-Za-z0-9]*)\{(\d+),(\d+)\}', line)
+            if valid_value is not None:
+                if splittedSprite.has_key(valid_value.group(1)):
+                    splittedSprite[valid_value.group(1)].append([int(valid_value.group(2)), int(valid_value.group(3))])
+                else:
+                    splittedSprite[valid_value.group(1)] = []
+                    splittedSprite[valid_value.group(1)].append([int(valid_value.group(2)), int(valid_value.group(3))])
+
+            else:
+                raise SyntaxError ('Coud not find sprite coords ' + line)
+        return splittedSprite
+
+
+
+
+
